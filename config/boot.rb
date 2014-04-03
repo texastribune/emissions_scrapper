65dae198072c44b61c8a2f38b4c6f11cd2ea21a7
malev <marcosvanetta@gmail.com>
2014-04-03 16:16:22 -0500

    config files

---
+++ config/boot.rb
@@ -0,0 +1,22 @@
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+APP_ROOT = File.expand_path("../..", __FILE__)
+TMP_DIR = "#{APP_ROOT}/tmp"
+
+$LOAD_PATH.unshift(APP_ROOT)
+Bundler.require(:default)
+
+LOGGER = Logger.new("#{APP_ROOT}/log/emissions.log")
+
+module Kernel
+  def logger
+    LOGGER
+  end
+end
+
+Mongoid.load!("config/mongoid.yml", :development)
+
+Dir["#{APP_ROOT}/lib/**/*.rb"].sort.each {|file| require file}
+Dir["#{APP_ROOT}/models/**/*.rb"].sort.each {|file| require file}
+
+puts "Loaded"
