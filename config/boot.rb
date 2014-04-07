f9b1747df0a5d297cac4f08bdf6741ebf1eef9a0
malev <marcosvanetta@gmail.com>
2014-04-07 11:42:39 -0500

    Environments

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
+APP_ENV = ENV['APP_ENV'] ||= "development" unless defined?(APP_ENV)
+
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
+require 'yaml'
+require 'active_support/inflector'
+
 APP_ROOT = File.expand_path("../..", __FILE__)
 TMP_DIR = "#{APP_ROOT}/tmp"
 
@@ -14,7 +19,7 @@ module Kernel
   end
 end
 
-Mongoid.load!("config/mongoid.yml", :development)
+Mongoid.load!("config/mongoid.yml", APP_ENV)
 
 Dir["#{APP_ROOT}/lib/**/*.rb"].sort.each {|file| require file}
 Dir["#{APP_ROOT}/models/**/*.rb"].sort.each {|file| require file}
