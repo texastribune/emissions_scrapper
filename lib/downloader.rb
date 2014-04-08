64fab86116ecbc3087d25f5073f219d387880e80
malev <marcosvanetta@gmail.com>
2014-04-07 21:32:56 -0500

    Reorganice arrays

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -2,11 +2,11 @@ module Downloader
   def call(from, to)
     logger.info("Files syncronization #{from}/#{to}")
 
-    threads = []
-    pages = []
     slices = (from..to).each_slice(40)
     pbar = ProgressBar.new("Sync", slices.count)
     slices.each do |slice|
+      threads = []
+      pages = []
       PageHTML.not_downloaded_batch(slice).each do |tracking_number|
         threads << Thread.new do
           pages << PageDownloader.new(tracking_number).call
@@ -16,7 +16,6 @@ module Downloader
       downloaded_pages = pages.select { |page| page.status == "done" }.map(&:to_hash)
       PageHTML.collection.insert(downloaded_pages)
       logger.info("#{downloaded_pages.length} pages has been stored")
-      pages = []
       pbar.inc
     end
   end
