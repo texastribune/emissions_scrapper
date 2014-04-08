ee7da029d36000abf81b8e51a7c4c9b22075edf2
malev <marcosvanetta@gmail.com>
2014-04-08 12:44:59 -0500

    Ensure scrapped=false on page_html models

--- models/page_html.rb
+++ models/page_html.rb
@@ -10,7 +10,15 @@ class PageHTML
   scope :done, where(status: 'done')
   scope :non_scrapped, where(scrapped: false)
 
+  before_create :ensure_scrapped_vale
+
   def self.not_downloaded_batch(list)
     list - self.in(tracking_number: list.map(&:to_s)).map(&:tracking_number)
+  end
+
+private
+
+  def ensure_scrapped_vale
+    self.scrapped = false
   end
 end
