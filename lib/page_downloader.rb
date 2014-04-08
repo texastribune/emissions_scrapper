8d038b31077e7f80a6d5afd93122469f4bb9ff09
malev <marcosvanetta@gmail.com>
2014-04-08 15:35:51 -0500

    Add user agent

--- lib/page_downloader.rb
+++ lib/page_downloader.rb
@@ -19,7 +19,7 @@ class PageDownloader
     content = ""
     status = "done"
     begin
-      content = open(url(tracking_number)).read
+      content = open(url(tracking_number), "User-Agent" => user_agent).read
       logger.info("#{tracking_number} has been downloaded")
     rescue OpenURI::HTTPError, Timeout::Error, SocketError, Errno::ECONNRESET => e
       content = e.message
@@ -31,5 +31,9 @@ class PageDownloader
 
   def url(tracking_number)
     "http://www11.tceq.texas.gov/oce/eer/index.cfm?fuseaction=main.getDetails&target=#{tracking_number}"
+  end
+
+  def user_agent
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.205 Safari/534.16"
   end
 end
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -8,6 +8,7 @@ module Scrapper
     per_page = 100.0
     total_pages = (total_documents/per_page).ceil
     pbar = ProgressBar.new("Importing", total_documents)
+    logger.info("Starting scrapping process for #{total_documents} documents")
     total_pages.times.each do |page|
       PageHTML.non_scrapped.limit(per_page).skip(page*per_page).each do |page|
         extracted_event = EmissionEventExtractor.new(page.content, page.tracking_number).call
