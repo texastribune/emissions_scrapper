be025c8dad73ff32c24a9f55e70545d77ae28930
malev <marcosvanetta@gmail.com>
2014-04-10 14:43:57 -0500

    Sequel configuration

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,11 @@
-require 'rubygems' unless defined?(Gem)
-require 'bundler/setup'
+APP_ENV  = ENV["APP_ENV"] ||= "development" unless defined?(APP_ENV)
 APP_ROOT = File.expand_path("../..", __FILE__)
 TMP_DIR = "#{APP_ROOT}/tmp"
+
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+require 'yaml'
+require 'sequel'
 
 $LOAD_PATH.unshift(APP_ROOT)
 Bundler.require(:default)
@@ -15,6 +19,10 @@ module Kernel
 end
 
 Mongoid.load!("config/mongoid.yml", :development)
+
+database_config = YAML.load_file('config/database.yml')
+database_config[APP_ENV]["logger"] = logger
+DB = Sequel.connect(database_config[APP_ENV])
 
 Dir["#{APP_ROOT}/lib/**/*.rb"].sort.each {|file| require file}
 Dir["#{APP_ROOT}/models/**/*.rb"].sort.each {|file| require file}
