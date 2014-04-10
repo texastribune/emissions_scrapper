84bf76e705155d5ae1391634d1a361f3d5211991
malev <marcosvanetta@gmail.com>
2014-04-10 14:44:25 -0500

    Adapt models with Sequel

--- models/page_html.rb
+++ models/page_html.rb
@@ -1,24 +1,25 @@
-class PageHTML
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  field :status, type: String
-  field :content, type: String
-  field :scrapped, type: Boolean, default: false
-  field :tracking_number, type: String
-
-  scope :done, where(status: 'done')
-  scope :non_scrapped, where(scrapped: false)
-
-  before_create :ensure_scrapped_vale
-
-  def self.not_downloaded_batch(list)
-    list - self.in(tracking_number: list.map(&:to_s)).map(&:tracking_number)
+class PageHTML < Sequel::Model
+  def self.not_downloaded_batch(tracking_numbers)
+    existing = dataset.where(tracking_number: tracking_numbers).
+                        select(:tracking_number).map(&:tracking_number)
+    tracking_numbers - existing
   end
 
-private
+  def self.insert(downloaded_pages)
+    self.dataset.multi_insert(downloaded_pages)
+  end
 
-  def ensure_scrapped_vale
-    self.scrapped = false
+  def self.non_scrapped_count
+    dataset.where(scrapped: false).count
+  end
+
+  def self.non_scrapped_paginate(page, per_page=100)
+    dataset.where(scrapped: false).
+            limit(per_page.to_i).
+            offset(((page-1)*per_page).to_i)
+  end
+
+  def scrapped!
+    update_fields({scrapped: true}, [:scrapped])
   end
 end
