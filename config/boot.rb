94a307219a9e0b8d9d90ccbad52b84109df48f51
malev <marcosvanetta@gmail.com>
2014-06-04 23:50:16 -0500

    Fix loging issue in boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
-APP_ENV  = ENV["APP_ENV"] ||= "development" unless defined?(APP_ENV)
+APP_ENV = ENV["APP_ENV"] ||= "development" unless defined?(APP_ENV)
+
 APP_ROOT = File.expand_path("../..", __FILE__)
 TMP_DIR = "#{APP_ROOT}/tmp"
 
@@ -23,8 +24,8 @@ module Kernel
   end
 end
 
+
 database_config = YAML.load_file('config/database.yml')
-database_config[APP_ENV]["logger"] = logger
 DB = Sequel.connect(database_config[APP_ENV])
 
 Dir["#{APP_ROOT}/lib/**/*.rb"].sort.each {|file| require file}
